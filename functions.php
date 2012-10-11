06c58c7677da2a3c9a3210ad55a172156940dbcf
Ce-yi <ceyiho@gmail.com>
2012-10-11 16:38:32 +0800

    CSS work. Initial.
    New style.css
    New theme images.
    Use Open Sans as fonts. Load Open Sans from Google Web Fonts API.

--- functions.php
+++ functions.php
@@ -13,11 +13,16 @@ function sundancemodern_setup() {
         '/inc/theme-options/theme-options.php',
         '/inc/theme-options/custom-color.php'
     );
-    
+
     foreach( $files as $file ) {
         require( get_stylesheet_directory() . $file );
     }
 }
 add_action( 'after_setup_theme', 'sundancemodern_setup' );
 
+
+function sundancemodern_scripts() {
+    wp_enqueue_style( 'open-sans-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' );
+}
+add_action( 'wp_enqueue_scripts', 'sundancemodern_scripts' );
 ?>
