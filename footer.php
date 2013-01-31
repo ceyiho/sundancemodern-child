71c78e47851ed2a4ad50e9f833338e912a76bc08
Ce-yi <ceyiho@gmail.com>
2013-01-31 15:22:52 +0800

    Changed author's link from automattic.com to ceyiho's GitHub page.

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<?php do_action( 'sundance_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'sundance' ); ?>" rel="generator"><?php printf( __( 'Powered by %s', 'sundance' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( '%1$s by %2$s', 'sundance' ), 'Sundance Modern', '<a href="http://automattic.com/" rel="designer">ceyiho</a>' ); ?>
+			<?php printf( __( '%1$s by %2$s', 'sundance' ), 'Sundance Modern', '<a href="https://github.com/ceyiho" rel="designer">ceyiho</a>' ); ?>
             <span class="sep"> | </span>
             <?php printf( __( 'A child theme of %1$s.', 'sundance' ), '<a href="http://wordpress.org/extend/themes/sundance/" rel="designer">Sundance</a>' ); ?>
 		</div><!-- .site-info -->
